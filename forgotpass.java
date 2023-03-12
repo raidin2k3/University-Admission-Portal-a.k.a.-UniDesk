75aa1fd8ee3232269b8f4bb5dea7481465da3c66
Pranshu Mishra <90717971+raidin2k3@users.noreply.github.com>
2023-03-12 19:43:33 +0530

    Update forgotpass.java

--- forgotpass.java
+++ forgotpass.java
@@ -207,8 +207,8 @@ public class forgotpass extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-        String fromEmail = "pranshumishra2003@yahoo.com";
-        String password = "tpouyiugqryfuokj";
+        String fromEmail = "email";
+        String password = "password";
         String toEmail = jTextField1.getText(); 
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.mail.yahoo.com");
